c8aafe706dd672fc84353ba6b0477e0088fc236e
Paul Chavard <pro@paul.chavard.net>
2023-10-13 21:24:57 +0200

    fix config

--- src/config.ts
+++ src/config.ts
@@ -1,30 +1,19 @@
-import { Type } from '@sinclair/typebox';
-import { Value } from '@sinclair/typebox/value';
+import { z } from 'zod';
 
-const ConfigSchema = Type.Object(
-  {
-    PORT: Type.Number({ default: 8080 }),
-    GEOJSON2PNG_TTL: Type.Number({ default: 1000 * 60 * 10 }),
-    GEOJSON2PNG_MAX: Type.Number({ default: 100 }),
-    GEOJSON2PNG_MAX_ITEM_SIZE: Type.Number({ default: 3_000_000 }),
-    GEOJSON2PNG_SECRET_TOKEN: Type.String({ minLength: 5 }),
-  },
-  { additionalProperties: true }
-);
-const Config = Value.Create(ConfigSchema);
-
-Object.keys(Config).forEach((key) => {
-  if (process.env[key]) {
-    Object.assign(Config, { [key]: process.env[key] });
-  } else if (key == 'GEOJSON2PNG_SECRET_TOKEN') {
-    Object.assign(Config, { GEOJSON2PNG_SECRET_TOKEN: '' });
-  }
+const Int = z.coerce.number().positive().int();
+const ConfigSchema = z.object({
+  PORT: Int.default(8080),
+  GEOJSON2PNG_TTL: Int.default(1000 * 60 * 10),
+  GEOJSON2PNG_MAX: Int.default(100),
+  GEOJSON2PNG_MAX_ITEM_SIZE: Int.default(3_000_000),
+  GEOJSON2PNG_SECRET_TOKEN: z.string().min(5),
 });
 
-if (!Value.Check(ConfigSchema, Config)) {
-  throw new Error(
-    [...Value.Errors(ConfigSchema, Config)].map((e) => e.message).join('\n')
-  );
+const result = ConfigSchema.safeParse(process.env);
+if (!result.success) {
+  const formatted = result.error.format();
+  throw new Error(formatted + '');
 }
+const Config = result.data;
 
 export { Config };
